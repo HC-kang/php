519f6424a6367a2a28d003efc1697d93f0b79716
Heechan Kang <weston0713@gmail.com>
2022-07-19 19:35:22 +0900

    24강

---
+++ programming_inflearn/functions/07_math/index.php
@@ -0,0 +1,31 @@
+<?php 
+
+// Absolute
+// echo abs(-1);
+
+
+//Floating Point
+// echo ceil(5.7);
+// echo floor(5.7);
+// echo round(5.7, 0);
+
+
+// hex -> dec
+// $bytes = random_bytes(32);
+// $hex = bin2hex($bytes);
+// echo hexdec($hex);
+// echo hexdec('F');
+
+// Max, Min
+// echo max(range(1, 5));
+// echo min(range(1, 5));
+
+// a^b
+// echo 2 ** 4;
+// echo pow(2, 4);
+
+// Create a random int
+// echo rand(0, 10);
+
+// Square root
+echo sqrt(16);
