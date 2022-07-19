ca120ea058d552d5acf4b182635d034988550212
Heechan Kang <weston0713@gmail.com>
2022-07-19 17:54:06 +0900

    21강

---
+++ programming_inflearn/functions/04_hash/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+// Get hash algorithms
+// var_dump(hash_algos());
+
+
+// Create a hash
+// $hash = hash('sha256', 'Hello, world');
+// echo $hash = crypt('Hello, world', 'secret');
+
+// Check a hash
+// var_dump(hash_equals($hash, hash('sha256', 'Hello, world')));
+
+// Hash Handler
+// $h = hash_init('sha256');
+// hash_update($h, 'Hello, world');
+
+// echo hash_final($h);
+
+// HMAC
+// Hash-based Message Authentication Code
+// key -> 'secret'
+// var_dump(hash_hmac_algos());
+
+// $hash = hash_hmac('sha256', 'Hello, world', 'secret');
+// echo hash_equals($hash, hash_hmac('sha256', 'Hello, world', 'secret'));
