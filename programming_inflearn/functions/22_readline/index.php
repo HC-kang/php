78ad94e09039260149cbc008d104d131c73fc80f
Heechan Kang <weston0713@gmail.com>
2022-07-20 16:07:45 +0900

    chap 42 readline

---
+++ programming_inflearn/functions/22_readline/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Read a line
+// $line = readline();
+// var_dump($line);
+
+
+// Add readline history
+// readline_add_history($line);
+
+
+// Clear history
+// readline_clear_history();
+
+
+// var_dump(readline_info());
+// var_dump(readline_list_history());
