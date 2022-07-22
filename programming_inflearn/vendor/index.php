9274271eac838fe31358f758b9dc5e998e564c59
Heechan Kang <weston0713@gmail.com>
2022-07-22 13:53:51 +0900

    chap 53 layouts

--- programming_inflearn/vendor/index.php
+++ programming_inflearn/vendor/index.php
@@ -2,4 +2,13 @@
 
 require_once 'bootstrap/app.php';
 
-echo 'Hello, world';
+// $_SESSION['user'] = true; // user test
+// unset($_SESSION['user']);
+
+?>
+
+<?php require_once 'layouts/top.php'?>
+
+<h1><?='Hello, world'?></h1>
+
+<?php require_once 'layouts/bottom.php'?>
