6391287e5f38790bd73569a2bbb542b4feaed813
Heechan Kang <weston0713@gmail.com>
2022-07-28 10:24:01 +0900

    chap 58 post-index

--- programming_inflearn/vendor/app.js
+++ programming_inflearn/vendor/app.js
@@ -10,6 +10,23 @@ if ($editor instanceof HTMLElement) {
         $form.addEventListener('submit', e => {
             const data = document.createTextNode(editor.getData());
             document.querySelector('#main__form-post textarea[name=content]').appendChild(data);
-        })
-    })
+        });
+    });
+}
+
+const $readmore = document.getElementById('readmore');
+if ($readmore instanceof HTMLElement) {
+    let page = 0;
+    $readmore.addEventListener('click', () => {
+        fetch('/?page=' + ++page, {method: 'get' }).then(async response => {
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(await response.text(), 'text/html');
+            const list = doc.querySelectorAll('.uk-container > .uk-list > li');
+            if (list.length > 0) {
+                Array.from(list).forEach(item => {
+                    document.querySelector('.uk-container > .uk-list').appendChild(item);
+                });
+            }
+        });
+    });
 }
