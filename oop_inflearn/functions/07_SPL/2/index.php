a49b7fe1a4eb1cd4402bb2ae559918d500800315
Heechan Kang <weston0713@gmail.com>
2022-07-30 18:19:25 +0900

    chap 18 spl

---
+++ oop_inflearn/functions/07_SPL/2/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// SplFileObject
+$file = new SplFileObject(dirname(__DIR__, 3) . '/README.md');
+var_dump($file->fread($file->getSize()));
+
+
+// SplFileInfo
+$fileinfo = $file -> getFileInfo();
+
+var_dump($fileinfo -> getBasename(), $fileinfo->isDir());
+
---
+++ oop_inflearn/functions/07_SPL/4/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// Classes Autoloading (PSR-4)
+// include './Classes/MyCLass.php';
+
+use Classes\MyClass;
+
+
+spl_autoload_register(function ($classname) {
+    include $classname . '.php';
+});
+
+
+// new MyClass();
+
+// var_dump(spl_autoload_functions());
+foreach (spl_autoload_functions() as $function) {
+    spl_autoload_unregister($function);
+}
