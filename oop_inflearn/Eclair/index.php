b183047857c2fefad59ed43af92d1bbad5e47c52
Heechan Kang <weston0713@gmail.com>
2022-07-31 14:25:10 +0900

    chap 26 routing

--- oop_inflearn/Eclair/index.php
+++ oop_inflearn/Eclair/index.php
@@ -1,6 +1,47 @@
 <?php
 
 require_once './vendor/autoload.php';
+
+use Eclair\Routing\Route;
+use Eclair\Database\Adaptor;
+use Eclair\Routing\Middleware;
+
+Adaptor::setup('mysql:dbname=myapp_test', 'root', 'root');
+
+class HelloMiddleware extends Middleware
+{
+    public static function process()
+    {
+        return true;
+    }
+}
+
+Route::add('get', '/', function () {
+    echo 'Hello, world';
+}, [ HelloMiddleware::class ]);
+
+Route::add('get', '/posts/{id}', function ($id) {
+    if ($post = Adaptor::getAll('SELECT * FROM posts WHERE `id` = ?', [ $id ] )) {
+        return var_dump($post);
+    }
+    http_response_code(404);
+});
+
+Route::run();
+
+
+
+// ---------------------------------------------------
+// use Eclair\Http\Request;
+
+// $_SERVER['REQUEST_METHOD'] = 'GET';
+// $_SERVER['PATH_INFO'] = '/posts/write';
+
+// var_dump(Request::getMethod());
+// var_dump(Request::getPath());
+
+
+// ---------------------------------------------------
 // require_once './src/Database/Adaptor.php';
 
 // use Eclair\Database\Adaptor;
@@ -14,11 +55,3 @@ require_once './vendor/autoload.php';
 
 // $posts = Adaptor::getAll('SELECT * FROM posts LIMIT 3', [], Post::class);
 // var_dump($posts);
-
-use Eclair\Http\Request;
-
-// $_SERVER['REQUEST_METHOD'] = 'GET';
-// $_SERVER['PATH_INFO'] = '/posts/write';
-
-// var_dump(Request::getMethod());
-var_dump(Request::getPath());
