e8d2c9c21c35ab8f979e043802c6960ce2612833
Heechan Kang <weston0713@gmail.com>
2022-07-31 13:46:59 +0900

    chap 25 http

--- oop_inflearn/Eclair/index.php
+++ oop_inflearn/Eclair/index.php
@@ -3,15 +3,22 @@
 require_once './vendor/autoload.php';
 // require_once './src/Database/Adaptor.php';
 
-use Eclair\Database\Adaptor;
+// use Eclair\Database\Adaptor;
 
-Adaptor::setup('mysql:dbname=myapp_test', 'root', 'root');
+// Adaptor::setup('mysql:dbname=myapp_test', 'root', 'root');
 
-class Post
-{
+// class Post
+// {
 
-}
+// }
 
+// $posts = Adaptor::getAll('SELECT * FROM posts LIMIT 3', [], Post::class);
+// var_dump($posts);
 
-$posts = Adaptor::getAll('SELECT * FROM posts LIMIT 3', [], Post::class);
-var_dump($posts);
+use Eclair\Http\Request;
+
+// $_SERVER['REQUEST_METHOD'] = 'GET';
+// $_SERVER['PATH_INFO'] = '/posts/write';
+
+// var_dump(Request::getMethod());
+var_dump(Request::getPath());
