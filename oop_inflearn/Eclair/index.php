28e20667e6f09d30461b2fcf453bcf8bd97ac1d0
Heechan Kang <weston0713@gmail.com>
2022-07-31 14:56:09 +0900

    chap 27 sessions

--- oop_inflearn/Eclair/index.php
+++ oop_inflearn/Eclair/index.php
@@ -2,33 +2,48 @@
 
 require_once './vendor/autoload.php';
 
-use Eclair\Routing\Route;
+use Eclair\Session\DatabaseSessionHandler;
 use Eclair\Database\Adaptor;
-use Eclair\Routing\Middleware;
 
 Adaptor::setup('mysql:dbname=myapp_test', 'root', 'root');
 
-class HelloMiddleware extends Middleware
-{
-    public static function process()
-    {
-        return true;
-    }
-}
+session_set_save_handler(new DatabaseSessionHandler());
 
-Route::add('get', '/', function () {
-    echo 'Hello, world';
-}, [ HelloMiddleware::class ]);
+session_start();
 
-Route::add('get', '/posts/{id}', function ($id) {
-    if ($post = Adaptor::getAll('SELECT * FROM posts WHERE `id` = ?', [ $id ] )) {
-        return var_dump($post);
-    }
-    http_response_code(404);
-});
+$_SESSION['message'] = 'Hello, world';
+$_SESSION['foo'] = new stdClass();
 
-Route::run();
+session_gc(10);
 
+
+// ---------------------------------------------------
+// use Eclair\Routing\Route;
+// use Eclair\Database\Adaptor;
+// use Eclair\Routing\Middleware;
+
+// Adaptor::setup('mysql:dbname=myapp_test', 'root', 'root');
+
+// class HelloMiddleware extends Middleware
+// {
+//     public static function process()
+//     {
+//         return true;
+//     }
+// }
+
+// Route::add('get', '/', function () {
+//     echo 'Hello, world';
+// }, [ HelloMiddleware::class ]);
+
+// Route::add('get', '/posts/{id}', function ($id) {
+//     if ($post = Adaptor::getAll('SELECT * FROM posts WHERE `id` = ?', [ $id ] )) {
+//         return var_dump($post);
+//     }
+//     http_response_code(404);
+// });
+
+// Route::run();
 
 
 // ---------------------------------------------------
